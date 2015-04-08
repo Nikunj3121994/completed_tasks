6b935aa7b806c4c9766a4da4e21c003e707e1a58
Александр Богут <chaotism@mail.ru>
2015-04-08 12:00:59 +0400

    сеттинги

--- demo/settings/base.py
+++ demo/settings/base.py
@@ -91,7 +91,7 @@ DATABASES = {
     # },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+        'NAME': os.path.join(PROJECT_ROOT, '../../db.sqlite3'),
     }
 
 }
