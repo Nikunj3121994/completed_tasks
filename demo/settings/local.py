cb9760bd0b13c8e69f48d382a78fdf9eb1658435
chaotism <chaotism@mail.ru>
2015-04-11 18:30:48 +0300

    sd;

--- demo/settings/local.py
+++ demo/settings/local.py
@@ -9,17 +9,17 @@ DEBUG = True
 MAX_USER_FILES_COUNT = 3
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    #    'NAME': 'test',
+    #    'USER': 'chaotism',  # 'postgres',
+    #    'PASSWORD': 'herotizm',  # 'postgres',
+    #    'HOST': 'localhost',
+    #    'PORT': '',
+    #},
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'test',
-        'USER': 'chaotism',  # 'postgres',
-        'PASSWORD': 'herotizm',  # 'postgres',
-        'HOST': 'localhost',
-        'PORT': '',
-    },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
-    # }
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+    }
 
 }
