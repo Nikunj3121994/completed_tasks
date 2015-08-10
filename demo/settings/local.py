d5e181cafadf7f9962370e919b29aff22720bcfd
chaotism <chaotism@mail.ru>
2015-08-10 23:55:02 +0300

    правки

--- demo/settings/local.py
+++ demo/settings/local.py
@@ -9,18 +9,21 @@ DEBUG = True
 MAX_USER_FILES_COUNT = 34
 MAX_DAYS_TO_OLD = 365
 
+
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.contrib.gis.db.backends.postgis',
-    #     'NAME': 'test',
-    #     'USER': 'chaotism',  # 'postgres',
-    #     'PASSWORD': 'herotizm',  # 'postgres',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
-    # },
     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'AUTOCOMMIT': True,
+    },
+    # 'default': {
+    #      'ENGINE': 'django.db.backends.sqlite3',
+    #      'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+    # }
 
 }
